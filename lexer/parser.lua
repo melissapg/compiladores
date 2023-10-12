f1110a79ce72e02c200f17a3f25a954faa5aa94f
Melissa <melissaguarilha@hotmail.com>
2023-10-12 01:55:24 -0300

    feat: parser

---
+++ lexer/parser.lua
@@ -0,0 +1,311 @@
+lexer = require("lexer")
+
+
+-- melhorar essa função
+function syntax_error(msg)
+    print(msg)
+    os.exit(1)
+end
+
+
+local parser =  {}
+
+function parser.init_parser(text)
+    lexer.init_lexer(text)
+    prox = lexer.get_next_token()
+end
+
+
+function peek(tag)
+    -- confere se um token eh o esperado
+    return (prox.tag == tag)
+end
+
+
+function eat(tag)
+    -- consome um token e atualiza p/ olhar o proximo
+    current_tag = prox.tag
+    if peek(tag) then
+        walk()
+        return current_tag
+    else
+        syntax_error("Encontrei um: "..current_tag.." esperava um :"..prox.tag)
+    end
+end
+
+
+function walk()
+    -- pega o proximo token
+    prox = lexer.get_next_token()
+    while prox.tag == 'SPACE' or prox.tag == 'COMMENT' or prox.tag == 'NEWLINE' do
+        prox = walk()
+    end
+end
+
+
+--[[Prog]]
+function parser.parseProg()
+    print("parseProg")
+    local block = parseBlock()
+    return {tag = 'Prog', block = {tag = "Bloco", block = block}}
+end
+
+
+--[[Cmd]]
+function parseCmd()
+    print("parseCmd")
+    if peek("do") then
+        eat("while")
+        local block = parseBlock()
+        eat("end")
+        return {tag = "CmdDo", block = {tag = "Bloco", block = block}}
+
+    elseif peek("while") then
+        eat("while")
+        local exp = parseExp()
+        eat("do")
+        local block = parseBlock()
+        eat("end")
+        return {tag = "CmdWhile", exp = exp, block = {tag = "Bloco", block = block}}
+
+    elseif peek("if") then
+        print("CmdIfElse")
+        eat("if")
+        local exp = parseExp()
+        eat("then")
+        local block = parseBlock()
+        local elses = parseElses()
+        return {tag = "CmdIfElse", exp = exp, block = {tag = "Bloco", block = block}, elses = elses}
+
+    elseif peek("local") then
+        eat("local")
+        local name = parseExpPrimaria()
+        eat("=")
+        local exp = parseExp()
+        return {tag = 'CmdLocal', name = name, exp = exp}
+
+    elseif peek("function") then
+        eat("function")
+        local name = parseExpPrimaria()
+        eat("(")
+        local params = Params()
+        eat(")")
+        local block = parseBlock()
+        eat("end")
+        return {tag = "CmdFunction", name = name, params = params, block = {tag = "Bloco", block = block}}
+
+    elseif peek("return") then
+        eat("return")
+        local exp = parseExp()
+        return {tag = "CmdReturn", exp = exp}
+
+    elseif peek("NOME") then
+        local name = parseExpPrimaria()
+        eat("=")
+        local exp = parseExp()
+        return {tag = "CmdAtribui", name = name, exp = exp}
+    end
+
+    while true do
+        local exp = parseExpSufixada()
+        eat("(")
+        local exps = Exps()
+        eat(")")
+        return {tag = "CmdChamada", exp = exp, exps = exps}
+    end
+end
+
+
+--[[Elses]]
+function parseElses()
+    while not(peek("end")) do
+        if peek("else") then
+            eat("else")
+            local block = parseBlock()
+            eat("end")
+            return {tag = "Bloco", block = block}
+        elseif peek("elseif") then
+            eat("elseif")
+            local exp = parseExp()
+            eat("then")
+            local block = parseBlock()
+            local elses = parseElses()
+            return {tag = "CmdIfElse", exp = exp, block = {tag = "Bloco", block = block}, elses = elses}
+        else
+            syntax_error("Esperava um else ou elseif.")
+        end
+    end
+    eat("end")
+    return nil
+end
+
+
+function get_prec(op)
+    if op == 'or' then
+        return 0
+    elseif op == 'and' then
+        return 1
+    elseif op == '<' or op == '>' or op == '<=' or op == '>=' or op == '==' or op == '~=' then
+        return 2
+    elseif op == '..' then
+        return 3
+    elseif op == '+' or op == '-' then
+        return 4
+    elseif op == '*' or op == '/' or op == '%' then
+        return 5
+    elseif op == 'not' or op == '#' or op == '-' then
+        return 6
+    elseif op == '(' or op == '[' then
+        return 7
+    else
+        return nil
+    end
+end
+
+
+--[[Exp]]
+function parseExp(min_prec)
+    min_prec = min_prec or 0
+
+    local e1 = nil
+    if peek('#') or peek('-') or peek('not') then
+        local op = eat(prox.tag)
+        local exp = parseExp(6)
+        e1 = {tag = 'ExpUn', op = op, exp = exp}
+    else
+        e1 = parseExpSimples()
+    end
+
+    while get_prec(prox.tag) and get_prec(prox.tag) >= min_prec do
+        local op = eat(prox.tag)
+        local e2 = parseExp(min_prec + 1)
+        e1 = {tag = 'ExpBin', op = op, e1 = e1, e2 = e2}
+    end
+    return e1
+end
+
+
+--[[ExpSufixada]]
+function parseExpSufixada()
+    local e = parseExpPrimaria()
+    while true do
+        if peek(".") then
+            eat(".")
+            e = parseExpPrimaria()
+            return {tag = 'ExpIndice', e = e}
+        elseif peek("[") then
+            eat("[")
+            e = parseExp(7)
+            eat("]")
+            return {tag = 'ExpIndice', e = e}
+        elseif peek("(") then
+            eat("(")
+            e = Exps()
+            eat(")")
+            return {tag = 'ExpChamada', e = e}
+        else
+            return e
+        end
+    end
+end
+
+
+--[[ExpSimples]]
+function parseExpSimples()
+    if peek("nil") then
+        eat("nil")
+        return {tag = 'ExpNil', val = nil}
+    elseif peek("true") or peek("false") then
+        bool = eat(prox.tag)
+        return {tag = 'ExpBool', val = bool}
+    elseif peek("NUMERO") then
+        number = eat('nil')
+        return {tag = 'ExpNum', val = number}
+    elseif peek("STRING") then
+        string = eat("STRING")
+        return {tag = 'ExpStr', val = string}
+    elseif peek("{") then
+        return parseTabela()
+    else
+        return parseExpSufixada()
+    end
+end
+
+
+--[[ExpPrimaria]]
+function parseExpPrimaria()
+    if peek("NOME") then
+        local name = eat("NOME")
+        return {tag = 'NOME', val = name}
+    elseif peek("(") then
+        return parseExp(7)
+    end
+end
+
+
+--[[Bloco]]
+function parseBlock()
+    print("parseBlock")
+    while not(peek("end") or peek("EOF") or peek("else") or peek("elseif")) do
+        parseCmd()
+    end
+end
+
+
+--[[Tabela]]
+function parseTabela()
+    eat("{")
+    local keyvals = {}
+    local int_key = 1
+    while not(peek("}")) do
+        if not(peek("NOME")) then
+            keyvals.insert(KeyVals(int_key))
+            int_key = int_key + 1
+        else
+            keyvals.insert(KeyVals())
+        end
+    end
+    eat("}")
+    return {tag = "ExpTabela", keyvals = keyvals}
+end
+
+
+--[[KeyVals]]
+function KeyVals(key)
+    key = key or nil
+    local exp = nil
+    if peek("NOME") then
+        local name = parseExpPrimaria()
+        eat("=")
+        exp = parseExp()
+        return {tag = 'KeyVal', key = name, val = exp}
+    else
+        exp = parseExp()
+        return {tag = 'KeyVal', key = key, val = exp}
+    end
+end
+
+
+--[[Params]]
+function Params()
+    local params = {}
+    while not(peek(")")) do
+        local name = parseExpPrimaria()
+        params.insert(name)
+    end
+    return {tag = "Params", params = params}
+end
+
+
+--[[Exps]]
+function Exps()
+    local exps = {}
+    while not(peek(")")) do
+        local exp = parseExp()
+        exps.insert(exp)
+    end
+    return {tag = "Exps", exps = exps}
+end
+
+
+return parser
