7a7d4cd82fa775300fd84e2e40b564375dbd6e58
Melissa <melissaguarilha@hotmail.com>
2023-11-20 19:21:50 -0300

    feat: intern function

--- compile/vm.py
+++ compile/vm.py
@@ -45,7 +45,117 @@ class Stack:
         return False if self.stack else True
 
 
-# class GlobalFunctions:
+class FunçaoInterna:
+    def index_error(self, func_name: str, args: list, n_args: int = 1):
+        more_args = f' or {n_args}' if n_args != 1 else ''
+        if len(args) < n_args:
+            print(f"TypeError: {func_name}() takes 1{more_args} argument(s).")
+            exit(True)
+
+    def print(self, args: list, out: str = ''):
+        out = ''.join(str(arg) for arg in args)
+        return print(out)
+
+    def tonumber(self, args: list):
+        self.index_error('tonumber', args)
+        try:
+            return int(args[0])
+        except ValueError:
+            print(f'ValueError: invalid literal for int() with base 10: {args[0]}.')
+
+    def tostring(self, args: list):
+        self.index_error('tostring', args)
+        return str(args[0])
+
+    def type(self, args: list):
+        self.index_error('type', args)
+        return type(args[0])
+
+    def string_byte(self, args: list):
+        self.index_error('string_byte', args, 2)
+
+        string = args[0]
+        idx = 1 if len(args) < 2 else args[1]
+        if not isinstance(string, str):
+            print(f"Error: Bad argument to 'byte' (string expected, got {type(string)})")
+            exit(True)
+        if idx >= 1 and idx <= len(string) and isinstance(idx, int):
+            return ord(string[idx - 1])
+        else:
+            return None
+
+    def string_len(self, args: list):
+        self.index_error('string_len', args)
+        if not isinstance(args[0], str):
+            print(f"Error: Bad argument to 'len' (string expected, got {type(args[0])})")
+            exit(True)
+        return len(args[0])
+
+    def string_rep(self, args: list):
+        self.index_error('string_rep', args, 2)
+        string = args[0]
+        rep = args[1]
+        if not isinstance(string, str):
+            print(f"Error: Bad argument to 'rep' (string expected, got {type(string)})")
+            exit(True)
+        if isinstance(rep, int):
+            return string * rep
+        else:
+            print(f"Error: Bad argument to 'rep' (number expected, got {type(rep)})")
+            exit(True)
+
+    def string_sub(self, args: list):
+        self.index_error('string_sub', args, 2)
+        string = args[0]
+        start = args[1]
+        end =  args[2] if len(args) == 3 else None
+        if not isinstance(string, str):
+            print(f"Error: Bad argument to 'sub' (string expected, got {type(string)})")
+            exit(True)
+        if isinstance(start, int) and (isinstance(end, int) or end == None): 
+            if not end:
+                return string[start - 1:]
+            return string[start - 1:end]
+        else:
+            print(f"Error: Bad argument to 'sub' (number expected, got {type(start), type(end)})")
+            exit(True)
+
+    def table_insert(self, args: list):
+        self.index_error('table_insert', args, 2)
+        table = args[0]
+        val = args[1]
+
+        if not isinstance(table, dict):
+            print(f"Error: Bad argument to 'table_insert' (dict expected, got {type(table)})")
+            exit(True)
+        table[len(table) + 1] = val
+        return table
+
+    def table_remove(self, args: list):
+        self.index_error('table_remove', args, 2)
+        table = args[0]
+        key = args[1]
+
+        if not isinstance(table, dict):
+            print(f"Error: Bad argument to 'table_remove' (dict expected, got {type(table)})")
+            exit(True)
+        if key in table:
+            del table[key]
+        return table
+
+    def all(self):
+        return {
+            "print": self.print,
+            "tonumber": self.tonumber,
+            "tostring": self.tostring,
+            "type": self.type,
+            "string_byte": self.string_byte,
+            "string_len": self.string_len,
+            "string_rep": self.string_rep,
+            "string_sub": self.string_sub,
+            "table_insert": self.table_insert,
+            "table_remove": self.table_remove
+        }
 
 
 def calc_exp(n1, n2, op):
@@ -76,8 +186,7 @@ def calc_exp(n1, n2, op):
 
 def eval(prog):
     stack = Stack()
-    # variables = GlobalFunctions().all()  # variáveis globais
-    variables = {"print": print}
+    variables = FunçaoInterna().all()  # variáveis globais
 
     for line in prog:
         instruction = line.split()[0]
@@ -134,7 +243,7 @@ def eval(prog):
 
             func = stack.pop_stack()
             if not func:
-                print(f"Error: Attempt to call a nil value (global '{func}').")
+                print(f"Error: The passed function doesn't exist.")
                 exit(True)
             stack.push_stack(func(args))
 
--- compile/vm_tests.lua
+++ compile/vm_tests.lua
@@ -1,2 +1,4 @@
-print("x")
-print("y")
+abcd= "abcd"
+a = table_insert({1,2,3, bota="fogo"}, 'a')
+print(a)
+print(table_remove(a, 'bota'))
